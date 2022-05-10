98465b931495a60d09608e4bbfab39c1aaf69658
Eoin O'Malley <eomalley@letsgetchecked.com>
2022-05-10 13:01:33 +0100

    Adjust grid for responsiveness

--- src/features/catalog/ProductList.tsx
+++ src/features/catalog/ProductList.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 export default function ProductList({ products }: Props) {
   return (
-    <Grid container spacing={4}>
+    <Grid container spacing={{ xs: 2, md: 3, lg: 4 }}>
       {products.map((product) => (
-        <Grid item xs={3} key={product.id}>
+        <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
           <ProductCard product={product} />
         </Grid>
       ))}
