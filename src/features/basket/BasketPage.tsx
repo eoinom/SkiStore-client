fcf12c51c5dd65fab68c8134ed11cef1ea3d970e
Eoin O'Malley <eomalley@letsgetchecked.com>
2022-11-04 11:59:19 +0000

    Update the Review component

--- src/features/basket/BasketPage.tsx
+++ src/features/basket/BasketPage.tsx
@@ -1,125 +1,17 @@
-import { Add, Delete, Remove } from '@mui/icons-material';
-import { LoadingButton } from '@mui/lab';
-import {
-  Box,
-  Button,
-  Grid,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from '@mui/material';
-import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
-import { currencyFormat } from '../../app/utils/currencyFormat';
-import { addBasketItemAsync, removeBasketItemAsync } from './basketSlice';
+import { Button, Grid, Typography } from '@mui/material';
+import { useAppSelector } from '../../app/store/configureStore';
 import BasketSummary from './BasketSummary';
+import BasketTable from './BasketTable';
 
 export default function BasketPage() {
-  const { basket, status } = useAppSelector((state) => state.basket);
-  const dispatch = useAppDispatch();
+  const { basket } = useAppSelector((state) => state.basket);
 
   if (!basket)
     return <Typography variant='h3'>Your basket is empty</Typography>;
 
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Products</TableCell>
-              <TableCell align='right'>Price</TableCell>
-              <TableCell align='center'>Quantity</TableCell>
-              <TableCell align='right'>Subtotal</TableCell>
-              <TableCell align='right'></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {basket.items.map((item) => (
-              <TableRow
-                key={item.productId}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component='th' scope='row'>
-                  <Box display='flex' alignItems='center'>
-                    <img
-                      src={item.pictureUrl}
-                      alt={item.name}
-                      style={{ height: 50, marginRight: 20 }}
-                    />
-                    <span>{item.name}</span>
-                  </Box>
-                </TableCell>
-                <TableCell align='right'>
-                  {currencyFormat(item.price)}
-                </TableCell>
-                <TableCell align='center'>
-                  <LoadingButton
-                    loading={
-                      status ===
-                      'pendingRemoveItem' + item.productId + '_reduce'
-                    }
-                    onClick={() =>
-                      dispatch(
-                        removeBasketItemAsync({
-                          productId: item.productId,
-                          quantity: 1,
-                          name: '_reduce',
-                        })
-                      )
-                    }
-                    color='error'
-                  >
-                    <Remove />
-                  </LoadingButton>
-                  {item.quantity}
-                  <LoadingButton
-                    loading={status === 'pendingAddItem' + item.productId}
-                    onClick={() =>
-                      dispatch(
-                        addBasketItemAsync({
-                          productId: item.productId,
-                          quantity: 1,
-                        })
-                      )
-                    }
-                    color='secondary'
-                  >
-                    <Add />
-                  </LoadingButton>
-                </TableCell>
-                <TableCell align='right'>
-                  {currencyFormat(item.quantity * item.price)}
-                </TableCell>
-                <TableCell align='right'>
-                  <LoadingButton
-                    loading={
-                      status ===
-                      'pendingDeleteItem' + item.productId + '_delete'
-                    }
-                    onClick={() =>
-                      dispatch(
-                        removeBasketItemAsync({
-                          productId: item.productId,
-                          quantity: item.quantity,
-                          name: '_delete',
-                        })
-                      )
-                    }
-                    color='error'
-                  >
-                    <Delete />
-                  </LoadingButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <BasketTable items={basket.items} isBasket={true} />
       <Grid container>
         <Grid item xs={6} />
         <Grid item xs={6}>
